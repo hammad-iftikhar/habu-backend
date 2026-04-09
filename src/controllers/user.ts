703fdc05751bdc5f47fe6992cd84e2756a96daa7
hammad-iftikhar <hammad.iftikhar723@gmail.com>
2026-04-10 00:33:46 +0500

    feat: add duplicate email check to user registration controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -13,6 +13,14 @@ class UserController {
         });
       }
 
+      const userExists = await UserRepository.findByEmail(email);
+
+      if (userExists) {
+        return res.status(400).json({
+          message: "User already exists. Please login",
+        });
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const user = await UserRepository.create({

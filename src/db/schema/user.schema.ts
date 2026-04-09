a4171037c00084f1cd3400ea02d688cc9984e982
hammad-iftikhar <hammad.iftikhar723@gmail.com>
2026-04-09 23:30:23 +0500

    feat: initialize Drizzle ORM with PostgreSQL and user schema definition

---
+++ src/db/schema/user.schema.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});

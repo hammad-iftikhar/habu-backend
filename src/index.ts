222f3025388e8ba83eb6e79a8bbf71591a72cf5e
hammad-iftikhar <hammad.iftikhar723@gmail.com>
2026-04-09 23:14:52 +0500

    feat: add cors middleware for development environments

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV == "development") {
+  app.use(cors());
+}
 
 export default app;
